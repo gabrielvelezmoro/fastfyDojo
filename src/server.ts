ec0794ce884ba8a14672d4ad809692582bbd16bc
gabriel <gabrieuvelezmoro@gmail.com>
2024-12-22 19:22:28 -0300

    feat: server running!

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import fastify from "fastify";
+
+const app = fastify({
+  logger: true,
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP Server running on http://localhost:3333");
+});
